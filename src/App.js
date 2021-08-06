7cf261bc233ed8e0c60dfe6b202be63d60b3db02
qadeer <kadeercs@gmail.com>
2021-08-06 22:56:05 +0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  let hulname = "my name is AQ"
+  return (
+    <div className="App">
+     
+    <h2>{hulname}</h2>
+    </div>
+  );
+}
+
+export default App;
