c1362cc49e4a2c3c7021ef213c62ca89ba39cbd1
qadeer <kadeercs@gmail.com>
2021-09-01 12:23:20 +0500

    expense chart

--- src/App.js
+++ src/App.js
@@ -6,25 +6,25 @@ import NewExpense from "./components/NewExpense/NewExpense";
 const DUMMY_EXPENSES = [
   {
     id: "e1",
-    date: new Date("2023.5.17"),
+    date: new Date("2021.5.17"),
     title: "BMW",
     price: 360000,
   },
   {
     id: "e2",
-    date: new Date("2018.7.25"),
+    date: new Date("2021.7.25"),
     title: "frari",
     price: 600000,
   },
   {
     id: "e3",
-    date: new Date("2022.6.19"),
+    date: new Date("2021.6.19"),
     title: "wagon",
     price: 460000,
   },
   {
     id: "e4",
-    date: new Date("2021.7.15"),
+    date: new Date("2020.7.15"),
     title: "honda",
     price: 560000,
   },
